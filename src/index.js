7aacd4581a0393d071f73992d765cc270f869d48
DanTheManGude <djg4755@rit.edu>
2018-03-01 01:01:53 -0500

    Very basic api calls

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', function (req, res) {
+  res.send('Got a GET request')
+})
 
-app.listen(3030, () => console.log('Example app listening on port 3030!'))
+app.post('/', function (req, res) {
+  res.send('Got a POST request')
+})
+
+app.put('/user', function (req, res) {
+  res.send('Got a PUT request at /user')
+})
+
+app.delete('/user', function (req, res) {
+  res.send('Got a DELETE request at /user')
+})
+
+app.listen(3030, () => console.log('Listening on port 3030!'))
