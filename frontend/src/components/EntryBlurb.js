0e40649c9515137c216b75eea373f1f784a4bb13
DanTheManGude <djg4755@rit.edu>
2018-08-11 17:25:26 -0400

    Chnage style of entry blurb

--- frontend/src/components/EntryBlurb.js
+++ frontend/src/components/EntryBlurb.js
@@ -4,7 +4,7 @@ export class EntryBlurb extends React.Component {
 
     render() {
         return (<span>
-            {this.props.state.date}&nbsp;{this.props.state.tldr}
+            {this.props.state.date} &nbsp; <strong> {this.props.state.tldr}</strong>
         </span>)
     }
 }
