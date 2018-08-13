e4b8a7c45e95ce51db4beb328580b43bdf494230
DanTheManGude <djg4755@rit.edu>
2018-08-13 17:15:01 -0400

    Remove dummy data for entries

--- src/server.js
+++ src/server.js
@@ -9,18 +9,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 var port = process.env.PORT || 3030;
 
 var placeHolderEntries = [
-    {   "time" : 216,
-        "date" : "Thursday Nov 23, 2017 7:43 PM",
-        "tldr" : "Thanksgiving with family",
-        "full" : "some more message"},
-    {   "time" : 423,
-        "date" : "Friday Dec 24, 2017 11:34 PM",
-        "tldr" : "Christmas Eve",
-        "full" : "some other message"},
-    {   "time" : 567,
-        "date" : "Saturday Jan 1, 2018 1:02 AM",
-        "tldr" : "New Year's at home",
-        "full" : "what do i even write in these things"}
   ]
 
 app.get('/api/hello', function (req, res) {
